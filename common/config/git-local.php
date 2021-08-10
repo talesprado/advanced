d9a8fdebe4b6b1dc3417a78b6d6d6631ef4800c1
Tales Prado <talesprado@gmail.com>
2021-08-10 00:33:26 -0300

    add config

---
+++ common/config/git-local.php
@@ -0,0 +1,12 @@
+<?php
+return [
+    'components' => [
+        'db' => [
+            'class' => 'yii\db\Connection',
+            'dsn' => 'mysql:host=localhost;dbname=vvs_test',
+            'username' => 'pradota',
+            'password' => '123581321',
+            'charset' => 'utf8',
+        ],
+    ],
+];
