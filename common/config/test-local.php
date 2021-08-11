07358d9130edaf084a587469619c1360b71a01dd
Tales Prado <talesprado@gmail.com>
2021-08-10 23:35:11 -0300

    Update test-local.php

--- common/config/test-local.php
+++ common/config/test-local.php
@@ -8,5 +8,13 @@ return [
             'password' => '123581321',
             'charset' => 'utf8',
         ],
+        'mailer' => [
+            'class' => 'yii\swiftmailer\Mailer',
+            'viewPath' => '@common/mail',
+            // send all mails to a file by default. You have to set
+            // 'useFileTransport' to false and configure a transport
+            // for the mailer to send real emails.
+            'useFileTransport' => true,
+        ],
     ],
 ];
