a5aaf11c8945a2416a5cec964da7b7e3ab656bd1
Tales Prado <talesprado@gmail.com>
2021-08-13 00:47:23 -0300

    Update git-local.php

--- common/config/git-local.php
+++ common/config/git-local.php
@@ -3,7 +3,7 @@ return [
     'components' => [
         'db' => [
             'class' => 'yii\db\Connection',
-            'dsn' => 'mysql:host=127.0.0.1:3306;dbname=vvs_test',
+            'dsn' => 'mysql:host=127.0.0.1;dbname=vvs_test',
             'username' => 'root',
             'password' => '123581321',
             'charset' => 'utf8',
